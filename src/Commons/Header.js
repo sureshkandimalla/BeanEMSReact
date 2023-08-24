dcd68d3e5974bcc2503b4d828613715b7e9e14fa
kvr5174 <33774847+kvr5174@users.noreply.github.com>
2023-08-24 23:08:36 +0530

    Create Header.js

---
+++ src/Commons/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FaUserTie } from 'react-icons/fa';
+import "./Header.scss";
+
+const Header = () => {
+    return (
+        <header>
+            <nav className='app-header-nav'>
+                <div className='left-wrapper'>
+                    <span><FaUserTie className="mr-2" /></span>
+                    <a href='/' className='nav-link'>Employee Management App</a>
+                </div>
+                <div className='right-wrapper'>
+                    <a href='/employees' className='nav-link'><span>Employee</span></a>
+                    <a href='/projects' className='nav-link'>Project</a>
+                    <a href='/assignments' className='nav-link'>Assignment</a>
+                </div>
+
+            </nav>
+        </header>
+
+    )
+}
+
+export default Header
