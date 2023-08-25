f665154500957cbd91f47d22c6456cd5f71002d1
RajeshwariKanchamreddy <kvr@gmail.com>
2023-08-26 00:08:30 +0530

    Adding validations for employee onboarding Form

--- src/Components/EmployeeOnBoarding/EmployeeOnBoarding.jsx
+++ src/Components/EmployeeOnBoarding/EmployeeOnBoarding.jsx
@@ -1,17 +1,54 @@
 import { Input, Form, Row, Col, Card, Radio, Button } from 'antd';
 import React, { useState, useRef } from 'react';
+import { validateEmail } from '../../utils';
 import './EmployeeOnBoarding.scss'
 
 
 
 const EmployeeOnBoardingForm = () => {
 
+    const [form] = Form.useForm();
+
+    const [formObj, setFormObj] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        designation: '',
+        dob: '',
+        gender: '',
+        address_line_1: '',
+        address_line_2: '',
+        city: '',
+        zipCode: ''
+    });
+
+    const handleFormSubmit = () => {
+        //api should be called here
+    }
+
+    const handleEmailValidation = () => {
+        if (!formObj.email.length) {
+            return Promise.reject(new Error('Email is required'))
+        }
+        else if (formObj.email.length && !validateEmail(formObj.email)) {
+            return Promise.reject(new Error('Invalid Email'));
+        }
+        return Promise.resolve();
+    }
+
+    const handleChange = (e, field) => {
+        const { value } = e.target;
+        let formsObj = { ...formObj };
+        formsObj[field] = value;
+        setFormObj(formsObj)
+    }
 
     return (
         <div className='employee-onboarding-form'>
             <h3 className='header'>Onboard Candidate(s)</h3>
             <Card className='employee-onboard-card'>
-                <Form
+                <Form onFinish={handleFormSubmit} form={form}
+
                 // labelCol={{
                 //     span: 7,
                 // }}
@@ -29,35 +66,35 @@ const EmployeeOnBoardingForm = () => {
                     </Row>
                     <Row gutter={30}>
                         <Col span={8} className='form-row'>
-                            <Form.Item label="First Name" name="firstName" rules={[{required: true}]}>
-                                <Input />
+                            <Form.Item label="First Name" name="First Name" rules={[{ required: true }]}>
+                                <Input onChange={(e) => handleChange(e, 'firstName')} value={formObj.firstName} />
                             </Form.Item>
                         </Col>
                         <Col span={8} className='form-row'>
-                            <Form.Item label="Last Name" name="lastName" rules={[{required: true}]}>
-                                <Input />
+                            <Form.Item label="Last Name" name="Last Name" rules={[{ required: true }]}>
+                                <Input onChange={(e) => handleChange(e, 'lastName')} value={formObj.lastName} />
                             </Form.Item>
                         </Col>
                         <Col span={8} className='form-row'>
-                            <Form.Item label="Designation" name="designation" rules={[{required: true}]}>
-                                <Input />
+                            <Form.Item label="Designation" name="Designation" rules={[{ required: true }]}>
+                                <Input onChange={(e) => handleChange(e, 'designation')} value={formObj.designation} />
                             </Form.Item>
                         </Col>
                     </Row>
                     <Row gutter={30}>
                         <Col span={8} className='form-row'>
-                            <Form.Item label="Email ID" name="emailID" rules={[{required: true}]}>
-                                <Input />
+                            <Form.Item label="Email ID" name="Email ID" rules={[{ required: true, message: '' }, { validator: handleEmailValidation }]}>
+                                <Input onChange={(e) => handleChange(e, 'email')} value={formObj.email} />
                             </Form.Item>
                         </Col>
                         <Col span={8} className='form-row'>
                             <Form.Item label="Date of Birth">
-                                <Input />
+                                <Input onChange={(e) => handleChange(e, 'dob')} value={formObj.dob} />
                             </Form.Item>
                         </Col>
                         <Col span={8} className='form-row'>
                             <Form.Item label="Gender">
-                                <Radio.Group>
+                                <Radio.Group onChange={e => handleChange(e, 'gender')} value={formObj.gender}>
                                     <Radio value="female">Female</Radio>
                                     <Radio value="male">Male</Radio>
                                     <Radio value="none">I choose not to disclose</Radio>
@@ -70,24 +107,24 @@ const EmployeeOnBoardingForm = () => {
                         <Row gutter={30}>
                             <Col span={8}>
                                 <Form.Item label="Address Line 1">
-                                    <Input />
+                                    <Input onChange={(e) => handleChange(e, 'address_line_1')} value={formObj.address_line_1} />
                                 </Form.Item>
                             </Col>
                             <Col span={8}>
                                 <Form.Item label="Address Line 2">
-                                    <Input />
+                                    <Input onChange={(e) => handleChange(e, 'address_line_2')} value={formObj.address_line_2} />
                                 </Form.Item>
                             </Col>
                         </Row>
                         <Row gutter={30}>
                             <Col span={8}>
                                 <Form.Item label="City, State">
-                                    <Input />
+                                    <Input onChange={(e) => handleChange(e, 'city')} value={formObj.city} />
                                 </Form.Item>
                             </Col>
                             <Col span={8}>
                                 <Form.Item label="Zip Code">
-                                    <Input />
+                                    <Input onChange={(e) => handleChange(e, 'zipCode')} value={formObj.zipCode} />
                                 </Form.Item>
                             </Col>
                         </Row>
@@ -96,37 +133,37 @@ const EmployeeOnBoardingForm = () => {
                         <h3>Employement Details</h3>
                         <Row gutter={30}>
                             <Col span={8}>
-                                <Form.Item label="Employement Type" name="employementType" rules={[{required: true}]}>
-                                    <Input />
+                                <Form.Item label="Employement Type" name="Employement Type" rules={[{ required: true }]}>
+                                    <Input onChange={(e) => handleChange(e, 'employementType')} value={formObj.employmentType} />
                                 </Form.Item>
                             </Col>
                             <Col span={8}>
-                                <Form.Item label="Tax Terms" name="taxterms" rules={[{required: true}]}>
-                                    <Input />
+                                <Form.Item label="Tax Terms" name="Tax Terms" rules={[{ required: true }]}>
+                                    <Input onChange={(e) => handleChange(e, 'taxTerms')} value={formObj.taxterms} />
                                 </Form.Item>
                             </Col>
                         </Row>
                         <Row gutter={30}>
                             <Col span={8}>
                                 <Form.Item label="Employement Start Date">
-                                    <Input />
+                                    <Input onChange={(e) => handleChange(e, 'empStartDate')} value={formObj.empStartDate} />
                                 </Form.Item>
                             </Col>
                             <Col span={8}>
                                 <Form.Item label="Employee ID">
-                                    <Input />
+                                    <Input onChange={(e) => handleChange(e, 'empID')} value={formObj.empID} />
                                 </Form.Item>
                             </Col>
                         </Row>
                         <Row gutter={30}>
                             <Col span={8}>
                                 <Form.Item label="Current Work Authorization Type">
-                                    <Input />
+                                    <Input onChange={(e) => handleChange(e, 'workAuthType')} value={formObj.workAuthType} />
                                 </Form.Item>
                             </Col>
                             <Col span={8} className='form-row'>
-                                <Form.Item label="Gender">
-                                    <Radio.Group>
+                                <Form.Item label="Billing Status">
+                                    <Radio.Group onChange={(e) => handleChange(e, 'billingStatus')} value={formObj.billingStatus}>
                                         <Radio value="billing">Billing</Radio>
                                         <Radio value="nonBilling">Non-Billing</Radio>
                                     </Radio.Group>
@@ -136,7 +173,7 @@ const EmployeeOnBoardingForm = () => {
                         <Row gutter={30}>
                             <Col span={8}>
                                 <Form.Item label="Wage rate(Offered Salary)">
-                                    <Input />
+                                    <Input onChange={(e) => handleChange(e, 'wageType')} value={formObj.wageType} />
                                 </Form.Item>
                             </Col>
                             <Col span={8} className='form-row'>
@@ -148,12 +185,12 @@ const EmployeeOnBoardingForm = () => {
                         <Row gutter={30}>
                             <Col span={8}>
                                 <Form.Item label="Department">
-                                    <Input />
+                                    <Input onChange={(e) => handleChange(e, 'department')} value={formObj.department} />
                                 </Form.Item>
                             </Col>
                             <Col span={8} className='form-row'>
                                 <Form.Item label="Reporting To">
-                                    <Input />
+                                    <Input onChange={(e) => handleChange(e, 'reportingTo')} value={formObj.reportingTo} />
                                 </Form.Item>
                             </Col>
                         </Row>
@@ -162,13 +199,13 @@ const EmployeeOnBoardingForm = () => {
                     <section>
                         <Row gutter={10} className='footer-btn-wrapper'>
                             <Col>
-                                <Button  type="primary">Back</Button>
+                                <Button type="primary">Back</Button>
                             </Col>
                             <Col>
                                 <Button>Cancel</Button>
                             </Col>
                             <Col>
-                                <Button  type="primary">Onboard</Button>
+                                <Button type="primary" onClick={() => form.submit()}>Onboard</Button>
                             </Col>
                         </Row>
                     </section>
---
+++ src/utils.js
@@ -0,0 +1,9 @@
+export const validateEmail = (value) => {
+    if (
+        /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/.test(
+            value
+        )
+    ) { return true; }
+
+    return false;
+};
